03c3f196a43f26dca99d812f8b306421c81ed839
Enrico B. Palma <enricob.palma@gmail.com>
2022-04-11 20:00:34 -0300

    middleware

---
+++ express-project/src/server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+
+const app = express();
+
+// app.use(express.json());
+
+const PORT = 3000;
+
+const friends = [
+  {
+    id: 0,
+    name: 'Albert Einstein',
+  },
+  {
+    id: 1,
+    name: 'Sir Isaac Newton',
+  }
+];
+
+app.use((req, res, next) =>{
+  const start = Date.now();
+
+  next();
+  const delta = Date.now() - start;
+  console.log(`${req.method} ${req.url} ${delta}`);
+});
+
+app.get('/friends', (req, res) => {
+  res.json(friends);
+});
+
+app.get('/friends/:friendId', (req, res) => {
+  const friendId = Number(req.params.friendId);
+  const friend = friends[friendId];
+
+  if (!friend) {
+    res.status(404).json({
+      error: 'Friend not found.'
+    });
+  };
+
+  res.status(200).json(friend);
+});
+
+app.get('/messages', (req, res) => {
+  res.status(200).send('<ul><li>Hello Alfred</li></ul>');
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server listening on http://localhost:${PORT}`);
+});
